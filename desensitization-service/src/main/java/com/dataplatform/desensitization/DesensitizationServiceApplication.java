abd294f21f0a5fa07ee19ec4590f7410b4791310
arcvideo_hgh <arcvideo_hgh@outlook.com>
2026-04-08 19:11:39 +0800

    Exclude DataSourceAutoConfiguration for services without database

--- desensitization-service/src/main/java/com/dataplatform/desensitization/DesensitizationServiceApplication.java
+++ desensitization-service/src/main/java/com/dataplatform/desensitization/DesensitizationServiceApplication.java
@@ -2,8 +2,9 @@ package com.dataplatform.desensitization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class DesensitizationServiceApplication {
 
     public static void main(String[] args) {
